34e699186d9b27a75faf35552c05f2480fe197ed
Johan Kristell <johan@jott.se>
2022-03-15 13:28:09 +0100

    defmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
 #![no_std]
 
+use core::sync::atomic::{AtomicUsize, Ordering};
+
 pub use hal::pac;
 pub use stm32f4xx_hal as hal;
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+static COUNT: AtomicUsize = AtomicUsize::new(0);
+defmt::timestamp!("{=usize}", {
+    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+    let n = COUNT.load(Ordering::Relaxed);
+    COUNT.store(n + 1, Ordering::Relaxed);
+    n
+});
+
+/// Terminates the application and makes `probe-run` exit with exit-code = 0
+pub fn exit() -> ! {
+    loop {
+        cortex_m::asm::bkpt();
+    }
+}
 
 mod led;
 pub use led::Led;
